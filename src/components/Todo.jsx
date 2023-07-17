9df023b8ccd0a9030097019fd6238ba1390f3387
Utpal Singhal <utpal.singhal499@gmail.com>
2023-07-17 23:12:21 +0530

    feat: delete button shake on hover

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 const Todo = ({ todo, toggleComplete, deleteTodo, enableEdit }) => {
+	const [isDeleteHovering, setIsDeleteHovering] = useState(false);
 	const checkboxChange = () => {
 		toggleComplete(todo.id);
 	};
@@ -12,6 +13,12 @@ const Todo = ({ todo, toggleComplete, deleteTodo, enableEdit }) => {
 	const handleEditButton = () => {
 		enableEdit(todo.id);
 	};
+  const handleMouseOver = () => {
+    setIsDeleteHovering(true);
+  }
+  const handleMouseOut = () => {
+    setIsDeleteHovering(false);
+  }
 
 	return (
 		<div className='todo'>
@@ -20,7 +27,7 @@ const Todo = ({ todo, toggleComplete, deleteTodo, enableEdit }) => {
 				onChange={checkboxChange}
 				checked={todo.isComplete}
 			/>
-			<span className={'task'+(todo.isComplete ? " todo-complete" : "")}>
+			<span className={"task" + (todo.isComplete ? " todo-complete" : "")}>
 				{todo.task}
 			</span>
 			{!todo.isComplete && (
@@ -28,14 +35,17 @@ const Todo = ({ todo, toggleComplete, deleteTodo, enableEdit }) => {
 					icon={faPenToSquare}
 					style={{ color: "#0f7b79" }}
 					onClick={handleEditButton}
-          className="edit-btn"
+					className='edit-btn'
 				/>
 			)}
 			<FontAwesomeIcon
 				icon={faTrashCan}
 				style={{ color: "#0f7b79" }}
 				onClick={handleDelete}
-        className="del-btn"
+				className='del-btn'
+				shake={isDeleteHovering ? true : false}
+				onMouseOver={handleMouseOver}
+				onMouseOut={handleMouseOut}
 			/>
 		</div>
 	);
