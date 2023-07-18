d7f97684c428d3173003cd5ded499bc496ab8f6d
Utpal Singhal <utpal.singhal499@gmail.com>
2023-07-19 03:20:35 +0530

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import TodoWrapper from "./components/TodoWrapper";
 
 function App() {
 	return (
 		<>
+			<Header />
 			<TodoWrapper />
 			<Footer />
 		</>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className='header'>
+			<p className='title'>EasyDo: A Todo app</p>
+		</div>
+	);
+};
+
+export default Header;
